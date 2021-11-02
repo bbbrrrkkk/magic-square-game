ec4c45624fcc288c2afec9e444781a3e8715fe3e
Eoin <eoin.larkin@gmail.com>
2021-11-02 18:52:27 +0000

    Javascript file to populate magic square

---
+++ assets/magic.js
@@ -0,0 +1,11 @@
+// Defining the base magic square
+// Function to be written to automatically generate the values array
+const values = [1,2,3,4,5,6,7,8,9];
+
+
+console.log(values)
+
+
+// Code to populate the magic square
+
+//
