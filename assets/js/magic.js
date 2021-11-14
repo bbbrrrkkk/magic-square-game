4efe517c88015a5da0f38848016f44c04f68ad10
Eoin <eoin.larkin@gmail.com>
2021-11-14 20:05:50 +0000

    Added logic to generate, map and remove values from a valid magic square

--- assets/js/magic.js
+++ assets/js/magic.js
@@ -1,8 +1,9 @@
 // Javascript for dynamic gird size
 //const button = document.querySelector('.my-button');
 const square = document.querySelector(".magic-square");
-//let SquareSize = 3; // Assumed Magic Square is size three by default
-
+let SquareSize = 3; // Assumed Magic Square is size three by default
+let SquareDiff = 1;
+let squareArraySorted = [3,5,7,4,9,2,8,1,6]; 
 
 /*
 button.addEventListener("click", handleClick);
@@ -13,20 +14,65 @@ document.getElementById('button-small-size').onclick = SetSquareSize;
 document.getElementById('button-medium-size').onclick = SetSquareSize;
 document.getElementById('button-large-size').onclick = SetSquareSize;
 
+/**
+ * Reads the button id and sets the square size parameter. reSize square function call to resize the Magic Square.
+ * @param {*} clicked 
+ */
 function SetSquareSize(clicked) {
     if (this.id == 'button-small-size') {
-        resizeSquare(3);
-        console.log(3);
+        SquareSize = 3;
+        makeSquare(genSquare());
+        resizeSquare(SquareSize);
+        console.log(SquareSize);
     }
     if (this.id == 'button-medium-size') {
-        resizeSquare(5);
-        console.log(5);
+        SquareSize = 5;
+        makeSquare(genSquare());
+        resizeSquare(SquareSize);
+        console.log(SquareSize);
     }
     if (this.id == 'button-large-size') {
-        resizeSquare(7);
-        console.log(7);
+        SquareSize = 7;
+        makeSquare(genSquare());
+        resizeSquare(SquareSize);
+        console.log(SquareSize);
     }
 }  
+
+/* storing the difficulty when the difficulty button is clicked */
+document.getElementById('button-diff-easy').onclick = SetSquareDiff;
+document.getElementById('button-diff-medium').onclick = SetSquareDiff;
+document.getElementById('button-diff-hard').onclick = SetSquareDiff;
+
+
+function SetSquareDiff(clicked) {
+    if (this.id == 'button-diff-easy') {
+        SquareDiff = 0;
+        makeSquare(genSquare());
+        resizeSquare(SquareSize);
+    }
+    if (this.id == 'button-diff-medium') {
+        SquareDiff= 2;
+        makeSquare(genSquare());
+        resizeSquare(SquareSize);
+    }
+    if (this.id == 'button-diff-hard') {
+        SquareDiff = 4;
+        makeSquare(genSquare());
+        resizeSquare(SquareSize);
+    }
+}  
+
+
+
+function genSquare() {
+    let foo = [];
+    for (var i = 1; i <= SquareSize**2; i++) {
+        foo.push(i+(SquareDiff*(i-1)))
+    }
+    console.log(foo);
+    return foo;
+}
 
 
 /**
@@ -34,45 +80,69 @@ function SetSquareSize(clicked) {
  * Size of the grid dynamically resized.
  * New grid size is specified as argument in the function call.
  */
-function resizeSquare(SquareSize) {
+function resizeSquare(NewSquareSize) {
   square.innerHTML = ''; /* Clearing HTML content */
-  for (let i = 0; i < SquareSize * SquareSize; i++) { /* Placing html content with for loop */
-    square.innerHTML +='<div class="square-item">' +i+'</div>';
+
+  IndexToRemove = getIndexToRemove(NewSquareSize);  
+
+  for (let i = 0; i < NewSquareSize * NewSquareSize; i++) { /* Placing html content with for loop */
+    if (IndexToRemove.includes(i)) {
+        square.innerHTML += "<div class='square-item'><input class='answer-box' type='number'></input></div>";
+        }
+    else {
+        square.innerHTML +='<div class="square-item">'+ squareArraySorted[i] + '</div>';}
   }
-  square.style.setProperty("--grid-size", SquareSize); /*Setting new grid size */
-  square.style.setProperty("font-size", (180/SquareSize)+'px'); /* Resizing the font size for the magic square */
+  square.style.setProperty("--grid-size", NewSquareSize); /*Setting new grid size */
+  square.style.setProperty("font-size", (180/NewSquareSize)+'px'); /* Resizing the font size for the magic square */
 }
 
-/*
+function makeSquare(squareArray){
+    // Defining the base magic square
+    // Function to be written to automatically generate the values array
 
-// Defining the base magic square
-// Function to be written to automatically generate the values array
+    // Filling the gird with the k values using the 
+    // MatLab version of the algorithm
+    var n = Math.sqrt(squareArray.length); // size of the grid
+    var i = 1; // starting row
+    var j = (n+1)/2; // starting column
 
-// Defining an empty square
-var square2 = [];
-for(var i=0; i<3; i++) {
-    square2[i] = new Array(3);
-}
-
-// Filling the gird with the k values using the 
-// MatLab version of the algorithm
-
-var n = 3; // size of the grid
-let array = [1,2,3,4,5,6,7,8,9]; // magic square array
-var i = 1; // starting row
-var j = (n+1)/2; // starting column
-
-for(var k=0; k<9; k++) {
-    let is = i;
-    let js = j;
-    square2[i-1][j-1] = array[k];
-    i = n - (n+1-i)%(n);
-    j = (j%n) + 1;
-    if (square2[i-1][j-1] != null) {
-        i = (is%n) + 1;
-        j = js;
+    // Defining an empty square
+    var square = [];
+    for(var i=0; i<n; i++) {
+        square[i] = new Array(n);
     }
+
+    // Filling the empty square with the array variables
+    for(var k=0; k<squareArray.length; k++) {
+        let is = i;
+        let js = j;
+        square[i-1][j-1] = squareArray[k];
+        i = n - (n+1-i)%(n);
+        j = (j%n) + 1;
+        if (square[i-1][j-1] != null) {
+            i = (is%n) + 1;
+            j = js;
+        }
+    }
+
+    // Flattening square to an array
+    squareArraySorted = square.flat();
+
+    console.table(square);
+    console.log(squareArraySorted);
+
 }
 
-console.table(square2);
-*/
+
+// Code to remove values:
+
+function getIndexToRemove(SquareSize){
+    let IndexToRemove = [];
+    
+    for(var i=0; i<SquareSize; i++){
+        IndexToRemove.push(parseInt(Math.random()*SquareSize**2));
+    }
+
+    return IndexToRemove;
+}
+
