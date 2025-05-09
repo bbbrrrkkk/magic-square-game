9d1789fad24ef525c9daebeb02f77b7cf4a3ef9a
bbbrrrkkk <aieksandr.frolov@ya.ru>
2025-05-09 12:47:00 +0700

    Update gcloud-deploy.sh

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,8 +1,8 @@
-DOMAIN="msg.larkin.ie"
+DOMAIN="msg.angelica.kuteikina"
 LOCAL_SITE_DIR="./"
 
 echo "uploading to cloud bucket"
 gsutil web set -m index.html -e 404.html gs://$DOMAIN
 
 echo "setting index and 404 pages"
-gsutil web set -m index.html gs://msg.larkin.ie
+gsutil web set -m index.html gs://msg.angelica.kuteikina
