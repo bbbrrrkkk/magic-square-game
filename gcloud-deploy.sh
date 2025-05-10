d9ca33ca7471708993b23f7f204bbec11c28df55
bbbrrrkkk <aieksandr.frolov@ya.ru>
2025-05-10 11:26:15 +0700

    Update gcloud-deploy.sh

--- gcloud-deploy.sh
+++ gcloud-deploy.sh
@@ -1,8 +1,8 @@
-DOMAIN="msg.angelica.kuteikina.ie"
+DOMAIN="angelica.kuteikina.ru"
 LOCAL_SITE_DIR="./"
 
 echo "uploading to cloud bucket"
 gsutil web set -m index.html -e 404.html gs://$DOMAIN
 
 echo "setting index and 404 pages"
-gsutil web set -m index.html gs://msg.angelica.kuteikina.ie
+gsutil web set -m index.html gs://angelica.kuteikina.ru
